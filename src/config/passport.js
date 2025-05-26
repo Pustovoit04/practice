26e995fe7ab51463735cfd55498804e282882869
Pustovoit04 <120163415+Pustovoit04@users.noreply.github.com>
2025-05-27 00:15:06 +0300

    Update passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -122,7 +122,7 @@ const GitHubStrategy = require('passport-github2').Strategy;
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/api/auth/github/callback'
+  callbackURL:'https://practice-trly.onrender.com/api/auth/github/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
